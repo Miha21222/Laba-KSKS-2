0980c2cc8277a6e55a60527a37983b7599966e4c
Miha21222 <67239835+Miha21222@users.noreply.github.com>
2023-11-03 18:39:47 +0200

    Add files via upload

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Laba_2_KSKS_Server;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+using System.Drawing;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ButDrawClick_ShouldClearDisplayInRed_WhenCommandIsClearDisplayWithRedColor()
+        {
+            Form1 form = new Form1();
+            form.recBox.Text = "clear display";
+            form.parBox.Text = "red";
+            Graphics g = form.DrawingPanel.CreateGraphics();
+            form.butDraw_Click(null, EventArgs.Empty);
+        }
+
+        [TestMethod]
+        public void ButDrawClick_ShouldClearDisplayInBlue_WhenCommandIsClearDisplayWithBlueColor()
+        {
+            Form1 form = new Form1(); 
+            form.recBox.Text = "clear display";
+            form.parBox.Text = "blue";
+            Graphics g = form.DrawingPanel.CreateGraphics();
+            form.butDraw_Click(null, EventArgs.Empty);
+        }
+
+        [TestMethod]
+        public void ButDrawClick_ShouldClearDisplayInGreen_WhenCommandIsClearDisplayWithGreenColor()
+        {
+            Form1 form = new Form1();
+            form.recBox.Text = "clear display";
+            form.parBox.Text = "green";
+            Graphics g = form.DrawingPanel.CreateGraphics();
+            form.butDraw_Click(null, EventArgs.Empty);
+        }
+    }
+}
